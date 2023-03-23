85888073c3e407a14475c286e4d305fddfc73f16
Tyler Long <tyler4long@gmail.com>
2023-03-23 09:51:07 -0700

    Write my own version of auto

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
-import { useProxy } from '@tylerlong/use-proxy';
-import React from 'react';
+import { releaseChildren, useProxy } from '@tylerlong/use-proxy';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Button, Space, Typography } from 'antd';
-import { auto } from '@tylerlong/use-proxy/lib/react';
+import { ProxyEvent } from '@tylerlong/use-proxy/lib/models';
+
+const auto = (render, props): JSX.Element => {
+  const [, refresh] = useState(false);
+  useEffect(() => {
+    const proxy = useProxy(props);
+    const listener = (event: ProxyEvent) => {
+      if (event.name === 'set') {
+        refresh((r) => !r);
+      }
+    };
+    proxy.__emitter__.on('event', listener);
+    return () => {
+      proxy.__emitter__.off('event', listener);
+      releaseChildren(proxy);
+    };
+  }, []);
+  return render();
+};
 
 const { Title, Text } = Typography;
 
@@ -14,28 +32,31 @@ const store = useProxy(new Store());
 
 const App = (props: { store: Store }) => {
   const { store } = props;
-  const render = () => (
-    <>
-      <Title>Untitled App</Title>
-      <Space>
-        <Button
-          onClick={() => {
-            store.count -= 1;
-          }}
-        >
-          -
-        </Button>
-        <Text>{store.count}</Text>
-        <Button
-          onClick={() => {
-            store.count += 1;
-          }}
-        >
-          +
-        </Button>
-      </Space>
-    </>
-  );
+  const render = () => {
+    console.log('render');
+    return (
+      <>
+        <Title>Untitled App</Title>
+        <Space>
+          <Button
+            onClick={() => {
+              store.count -= 1;
+            }}
+          >
+            -
+          </Button>
+          <Text>{store.count}</Text>
+          <Button
+            onClick={() => {
+              store.count += 1;
+            }}
+          >
+            +
+          </Button>
+        </Space>
+      </>
+    );
+  };
   return auto(render, props);
 };
 
