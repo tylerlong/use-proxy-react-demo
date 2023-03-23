6629861f585b3f49618337d84198731a160d6037
Tyler Long <tyler4long@gmail.com>
2023-03-23 09:37:55 -0700

    Simplify code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,45 @@
-import React, { StrictMode } from 'react';
+import { useProxy } from '@tylerlong/use-proxy';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Button, Space, Typography } from 'antd';
+import { auto } from '@tylerlong/use-proxy/lib/react';
 
-import App from './app';
-import store from './store';
+const { Title, Text } = Typography;
+
+class Store {
+  public count = 0;
+}
+
+const store = useProxy(new Store());
+
+const App = (props: { store: Store }) => {
+  const { store } = props;
+  const render = () => (
+    <>
+      <Title>Untitled App</Title>
+      <Space>
+        <Button
+          onClick={() => {
+            store.count -= 1;
+          }}
+        >
+          -
+        </Button>
+        <Text>{store.count}</Text>
+        <Button
+          onClick={() => {
+            store.count += 1;
+          }}
+        >
+          +
+        </Button>
+      </Space>
+    </>
+  );
+  return auto(render, props);
+};
 
 const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
-root.render(
-  <StrictMode>
-    <App store={store} />
-  </StrictMode>,
-);
+root.render(<App store={store} />);
--- src/store.ts
+++
@@ -1,9 +0,0 @@
-import { useProxy } from '@tylerlong/use-proxy';
-
-export class Store {
-  public count = 0;
-}
-
-const store = useProxy(new Store());
-
-export default store;
